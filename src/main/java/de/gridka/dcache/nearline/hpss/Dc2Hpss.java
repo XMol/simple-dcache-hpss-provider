41a16c46c1779ad141f614f85facd8936f140fa9
Xavier Mol <xavier.mol@kit.edu>
2016-08-12 13:07:48 +0200

    Fix syntax error in String construction.

--- src/main/java/de/gridka/dcache/nearline/hpss/Dc2Hpss.java
+++ src/main/java/de/gridka/dcache/nearline/hpss/Dc2Hpss.java
@@ -138,7 +138,7 @@ public class Dc2Hpss extends AbstractBlockingNearlineStorage
   // Optionally evaluate any information of the file to construct the path.
   private Path getExternalPath(StorageInfo storageInfo, String pnfsId)
   {
-    return Paths.get(mountpoint.toString() + '/' pnfsId);
+    return Paths.get(mountpoint.toString() + '/' + pnfsId);
   }
   
   @Override
